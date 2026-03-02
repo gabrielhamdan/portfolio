a2b19ece886c1eeeb4a7b1a00f1f5de799754fac
Gabriel Hamdan <gabriel_hamdan@hotmail.com>
2026-03-01 22:45:21 -0300

    fix: redirecting to about so navbutton is automatically selected

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
@@ -7,7 +7,7 @@ export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />}>
-                <Route index element={<About />} />
+                <Route index element={<Navigate to="about" replace />} />
                 <Route path="about" element={<About />} />
                 <Route path="projects" element={<Projects />} />
             </Route>
