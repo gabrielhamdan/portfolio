0d5ed6c0ac04b4d06a446cdb2532b63bfc96e91e
Gabriel Hamdan <gabriel_hamdan@hotmail.com>
2026-03-03 08:21:53 -0300

    style: adding mobile responsivity

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,32 +1,35 @@
 import { useLanguage } from "../../contexts/LanguageContext";
+import "./Grettings.css";
 
 export default function Greetings() {
     const { language } = useLanguage();
 
     return (
         <>
-            {
-                language === "pt" ?
-                    <div>
-                        <h3>olá,</h3>
-                        <h1>sou <span className="bold">Gabriel Hamdan</span></h1>
-                        <h2>desenvolvedor</h2>
-                    </div>
+            <div className="greetings-container">
+                {
+                    language === "pt" ?
+                        <>
+                            <h3>olá,</h3>
+                            <h1>sou <span className="bold">Gabriel Hamdan</span></h1>
+                            <h2>desenvolvedor</h2>
+                        </>
 
-                    : language === "en" ?
-                        <div>
-                            <h3>hello,</h3>
-                            <h1>I'm <span className="bold">Gabriel Hamdan</span></h1>
-                            <h2>developer</h2>
-                        </div>
+                        : language === "en" ?
+                            <>
+                                <h3>hello,</h3>
+                                <h1>I'm <span className="bold">Gabriel Hamdan</span></h1>
+                                <h2>developer</h2>
+                            </>
 
-                        :
-                        <div>
-                            <h3>salut,</h3>
-                            <h1>je suis <span className="bold">Gabriel Hamdan</span></h1>
-                            <h2>développeur</h2>
-                        </div>
-            }
+                            :
+                            <>
+                                <h3>salut,</h3>
+                                <h1>je suis <span className="bold">Gabriel Hamdan</span></h1>
+                                <h2>développeur</h2>
+                            </>
+                }
+            </div>
         </>
     );
 }
